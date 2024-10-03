32325ac04610cf41cb6f43e4d9fd37c6fc126c3b
David Alves Dutra <dadutra@hotmail.com>
2024-10-03 08:14:40 -0300

    User mock created. Db works.

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,7 +1,13 @@
 import express from "express";
+import * as db from "./src/config/initialData.js"
+
 
 const app = express();
 const env = process.env;
+const PORT = env.PORT || 8080;
+
+
+db.createInitialData();
 
 app.get('/api/status', (req, res) => {
     return res.status(200).json({
@@ -11,5 +17,4 @@ app.get('/api/status', (req, res) => {
     }) 
 });
 
-const PORT = env.PORT || 8080;
 app.listen(PORT, () => {console.info(`Server started successfully at port: ${PORT}`)})
