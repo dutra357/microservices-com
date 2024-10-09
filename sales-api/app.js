bd6e69e8aef87ca7349d96037a4e6a9cb26e8513
David Alves Dutra <dadutra@hotmail.com>
2024-10-09 13:48:04 -0300

    Mock data users and products. Sale api with ongo. Mongo configured.

--- sales-api/app.js
+++ sales-api/app.js
@@ -1,20 +1,29 @@
 import express from "express";
 
+import { startInitialData } from "./src/config/db/mockData.js"
 import { connect } from "./src/config/db/mongoConfig.js";
+import Order from "./src/modules/sales/model/Order.js";
+
+
 
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8082
 
 connect();
+startInitialData()
 
-app.get('/api/status', (req, res) => {
+app.get('/api/status', async (req, res) => {
+    let teste = await Order.find();
+    console.log(teste);
+
     return res.status(200).json({
         service: "Sales-API",
         status: "UP",
         httpStatus: 200
     })
-})
+});
+
 
 app.listen(PORT, () => {
     console.info(`Server started successfully at port: ${PORT}`)
